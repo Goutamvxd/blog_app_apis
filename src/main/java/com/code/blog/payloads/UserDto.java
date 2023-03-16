e87a16d21f1b3ad218c299a42a44a18f81f85763
Goutamvxd <vantegoutam@gmail.com>
2023-03-16 20:13:46 +0530

    Exception Handling

--- src/main/java/com/code/blog/payloads/UserDto.java
+++ src/main/java/com/code/blog/payloads/UserDto.java
@@ -1,5 +1,16 @@
 package com.code.blog.payloads;
 
+
+
+
+
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,9 +21,20 @@ import lombok.Setter;
 public class UserDto {
 
 	private int id;
+	
+	@NotEmpty
+	@Size(min = 2,message="user name must be min of 2 character")
 	private String name;
+	
+	@Email(message = "Invaid Email")
 	private String email;
+	
+	@NotEmpty
+	@Size(min=5,max=10,message="password must be more than 3 characters and less than 10 characters")
+	@Pattern(regexp = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\\w\\s]).{8,}$/")
 	private String password;
+	
+	@NotEmpty
 	private String about;
 	
 }
